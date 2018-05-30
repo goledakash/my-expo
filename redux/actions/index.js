0819cead0397d1570af1e86e6e4455ae7645c30d
Akash Goled <akashgoled@Akashs-MacBook-Pro.local>
2018-05-29 21:04:16 -0400

    initial commit

---
+++ redux/actions/index.js
@@ -0,0 +1,11 @@
+import * as Type from './ActionTypes';
+
+export const loginUser =(username, password)=>{
+  return {
+    type:Type.LOGIN_USER,
+    username,
+    password
+  }
+
+  //return object 
+}
